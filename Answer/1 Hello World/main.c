491d15ac1f17b570197f695012c8049c7b46ec4a
aomsir <info@say521.cn>
2020-11-28 13:02:58 +0800

    Create main.c

---
+++ Answer/1 Hello World/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  1 Hello World
+//
+//  Created by 奥秘Sir on 2020/11/28.
+//
+
+#include <stdio.h>
+
+int main(void) {
+    // insert code here...
+    printf("Hello World!\n");
+    return 0;
+}

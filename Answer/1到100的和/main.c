b12a21543d1b4ba75a440b046518bd8bfc0f0142
aomsir <info@say521.cn>
2020-11-28 13:02:56 +0800

    Create main.c

---
+++ Answer/1到100的和/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  1到100的和
+//
+//  Created by 黄祥河 on 2020/11/28.
+//
+
+#include <stdio.h>
+
+int main(void) {
+    int i;
+    int sum=0;
+    for(i=0;i<=100;i++){
+        sum+=i;
+        }
+    printf("1到100的和是：%d\n",sum);
+    return 0;
+}

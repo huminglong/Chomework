b7594584379204ade48c7b4c59a7cde8b953dc60
aomsir <info@say521.cn>
2021-02-07 09:59:24 +0800

    1

---
+++ Note/2月/2.6/happy_year.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+void H(void);
+void A(void);
+void P(void);
+void Y(void);
+void N(void);
+void E(void);
+void W(void);
+void A(void);
+void R(void);
+
+int main(void){
+    H();
+    A();
+    P();
+    P();
+    Y();
+    printf(" ");
+    N();
+    E();
+    W();
+    printf(" ");
+    Y();
+    E();
+    A();
+    R();
+    printf("!\n");
+    
+    return 0;
+    
+    }
+    
+void H(void){
+    printf("H");
+}
+    
+void A(void){
+    printf("A");
+}
+    
+void P(void){
+    printf("P");
+}
+    
+void Y(void){
+    printf("Y");
+}
+    
+void N(void){
+    printf("N");
+}
+    
+void E(void){
+    printf("E");
+}
+
+void W(void){
+     printf("W");
+}
+    
+void R(void){
+    printf("R");
+    }

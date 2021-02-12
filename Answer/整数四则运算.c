827f6cebf78e8fc603aa5b67be3ec6fb36fe78d9
aomsir <info@say521.cn>
2021-02-12 20:22:33 +0800

    1

---
+++ Answer/整数四则运算.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void){
+	int A,B;
+	scanf("%d%d",&A,&B);
+	printf("%d + %d = %d\n",A,B,A+B);
+	printf("%d - %d = %d\n",A,B,A-B);
+	printf("%d * %d = %d\n",A,B,A*B);
+	printf("%d / %d = %d\n",A,B,A/B);
+
+	return 0;
+
+}

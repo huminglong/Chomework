1298a35c93b7cb413a46af9bd81c0957125eeb4a
aomsir <info@say521.cn>
2021-02-27 21:15:46 +0800

    Answer

--- Answer/计算火车运行时间.c
+++ Answer/计算火车运行时间.c
@@ -2,8 +2,35 @@
 
 int main(void){
 	int start,end;
+	int a,b,c,d;
+	int hour,min;
 	printf("格式：1201 1530\n");
 	printf("请按照格式输入出发时间和到达时间:");
-	scanf("%d %d",&hh,&mm);
+	scanf("%d %d",&start,&end);
+	a = start/100;
+	b = end/100;
+	c = start-a*100;
+	d = end-b*100;
 
+	if(d > 60 || d < 0){
+		printf("请重新输入\n");
+	}
+
+	if(a > b){
+		printf("请重新输入\n");
+	}
+
+	if(c > d){
+		hour = b - a - 1;
+		min = 60 - c + d;
+		printf("%d:%d",hour,min);
+	}
+
+	if(c < d){
+		hour = b - a;
+		min = d - c;
+		printf("%d:%d",hour,min);
+	}
+
+	return 0;
 }

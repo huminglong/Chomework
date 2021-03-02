d8dd400c47a89fb8e242e5713634226c8e7573ae
aomsir <info@say521.cn>
2021-03-02 21:29:13 +0800

    Create 1-100所有素数.c

---
+++ Answer/1-100所有素数.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int prime(int n);
+
+
+int main(void)
+{
+	
+	int i;
+	for(i=2;i<100;i++)
+	{
+		if(prime(i) == 1)
+		{
+			printf("%d",i)
+		}
+	}
+
+}
+
+int prime(int n)
+{
+	int i;
+	for(i=2;i<n;i++)
+	{
+		if(n%i == 0)
+		{
+			return 0;
+		}
+		
+	}
+
+	return 1;
+
+}

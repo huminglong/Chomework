9f1e2ca8e1fd6faf2205563374b390facbd25863
aomsir <info@say521.cn>
2021-03-08 19:13:09 +0800

    1

--- Answer/阶梯电价.c
+++ Answer/阶梯电价.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
 int main(void){
-	int i;
-	int
+	int UseCount;
+	float cost;
+	printf("请输入您本月的用电量：");
+	scanf("%d",&UseCount);
+
+	if(UseCount <= 50 && UseCount >= 0){
+		cost = 0.53 * UseCount;
+	}
+	else{
+		cost = 0.53 * 50 + (UseCount - 50) * (0.53 + 0.05); 
+	}
+
+	printf("您本月的电费是:%.2f\n",cost);
+
+
+	return 0;
 }

8d51ee8f147cc8a88b0fb997b81df5b4cdc9fc38
aomsir <info@say521.cn>
2021-03-10 18:16:31 +0800

    1

---
+++ Answer/猜数字游戏.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+	int RandomNumber = rand()%100;   //生成随机数RandomNumber
+	int count = 0;  //猜数字的次数
+	int UserNumber;
+
+
+	do{
+		printf("请输入一个您猜想的数字:\n");
+		scanf("%d",&UserNumber);
+		if(UserNumber > RandomNumber)
+		{
+			printf("Too big\n");
+			count++
+		}
+		else if(UserNumber > RandomNumber)
+		{
+			printf("Too small\n");
+			count++;
+		}
+		else
+		{
+			count++;
+			break;
+		}
+	}while(RandomNumber == UserNumber);
+
+
+	if (count == 1)
+	{
+		printf("Bingo\n");
+	}
+	else if(count > 1 && count <= 3)
+	{
+		printf("Lucky You\n");
+	}
+	else if(count > 3 && count < )
+	{
+
+	}
+	else
+	{
+		printf("Game Over\n");
+	}
+
+
+	
+
+
+}
